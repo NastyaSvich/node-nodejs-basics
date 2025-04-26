1f7e80ac6504e0a59dfed835bb6537d037f07b95
ASvich <nastya_drakosha@bk.ru>
2025-04-26 21:15:53 +0300

    feat: implement file system

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,20 @@
+import {existsSync} from "fs";
+import fs from "node:fs/promises";
+import {fileURLToPath} from "url";
+import path from "path";
+
 const copy = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+
+    const sourceDirPath = path.resolve(__dirname, "files");
+    const destinationDirPath = path.resolve(__dirname, "files_copy");
+
+    if (!existsSync(sourceDirPath) || existsSync(destinationDirPath)) {
+        throw new Error("FS operation failed");
+    } else {
+        await fs.cp(sourceDirPath, destinationDirPath, {recursive: true});
+    }
 };
 
 await copy();
